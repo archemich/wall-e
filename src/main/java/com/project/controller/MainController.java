4f1235d11199c85b8b5ddb3eb5bd6eeaae2f8e5f
Dmitrii <drelagreen@pop-os.localdomain>
2021-04-06 15:58:46 +0300

    add terminal

--- src/main/java/com/project/controller/MainController.java
+++ src/main/java/com/project/controller/MainController.java
@@ -3,6 +3,7 @@ package com.project.controller;
 import com.project.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,9 +18,6 @@ import java.util.UUID;
 
 @Controller
 public class MainController {
-    @Value("${upload.path}")
-    private String uploadpath;
-
     @GetMapping("/")
     public String greeting(Map<String, Object> model){
         return "main";
@@ -33,33 +31,41 @@ public class MainController {
         model.addAttribute("url","/main");
         return "main";
     }
-
-    @PostMapping("/main")
-    public String add(
-            @AuthenticationPrincipal User user,
-            @RequestParam(value = "file", required = true) MultipartFile file,
-            BindingResult bindingResult,
-            Model model
-    ) throws IOException {
-        saveFile(file);
-        model.addAttribute("url","/main");
-        return "main";
+    @GetMapping("/terminal")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String terminal(
+            Model model,
+            @AuthenticationPrincipal User user
+    ){
+        model.addAttribute("url","/terminal");
+        return "terminal";
     }
+//    @PostMapping("/main")
+//    public String add(
+//            @AuthenticationPrincipal User user,
+//            @RequestParam(value = "file", required = true) MultipartFile file,
+//            BindingResult bindingResult,
+//            Model model
+//    ) throws IOException {
+//        saveFile(file);
+//        model.addAttribute("url","/main");
+//        return "main";
+//    }
 
-    private void saveFile(MultipartFile file) throws IOException {
-        if (file != null && !file.isEmpty() && file.getSize()!=0 && !file.getOriginalFilename().isEmpty() && !file.getOriginalFilename().equals("") && !file.getOriginalFilename().equals(" ")) {
-            File uploadDir = new File(uploadpath);
-
-            if (!uploadDir.exists()) {
-                uploadDir.mkdir();
-            }
-            String uuidFile = UUID.randomUUID().toString();
-
-            String resultFileName = uuidFile + "." +  file.getOriginalFilename();
-
-            file.transferTo(new File(uploadpath + "/" + resultFileName));
-        }
-    }
+//    private void saveFile(MultipartFile file) throws IOException {
+//        if (file != null && !file.isEmpty() && file.getSize()!=0 && !file.getOriginalFilename().isEmpty() && !file.getOriginalFilename().equals("") && !file.getOriginalFilename().equals(" ")) {
+//            File uploadDir = new File(uploadpath);
+//
+//            if (!uploadDir.exists()) {
+//                uploadDir.mkdir();
+//            }
+//            String uuidFile = UUID.randomUUID().toString();
+//
+//            String resultFileName = uuidFile + "." +  file.getOriginalFilename();
+//
+//            file.transferTo(new File(uploadpath + "/" + resultFileName));
+//        }
+//    }
 
 
 
