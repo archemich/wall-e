1c1fe3285baeb782fe1dbbcc0db37e6276b7387e
FriZIk <nesterenko251100@gmail.com>
2021-05-26 23:35:08 +0300

    prepare to merge

--- site/src/main/java/com/project/reps/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.project.reps;
-
-import com.project.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
